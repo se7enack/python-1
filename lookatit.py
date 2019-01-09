58a308a62a8049cb53afc01bef27a56b3dd16ed3
dangerousness <webmaster@dorkcloud.com>
2019-01-08 21:47:31 -0500

    made into a function

--- lookatit.py
+++ lookatit.py
@@ -4,18 +4,21 @@
 
 import webbrowser, os
 
-file="/etc/hosts"
+this="/etc/hosts"
 
-try:
-    if os.path.exists(file):
-        if os.path.isfile(file):
-            print(open(file).read())
-        else:
-            print(file + " exists, however it is not a file (looks more like a folder to me).")
-            print("Correct the path to point to an actual file.")
-    elif not os.path.exists(file):
-        print(file + " is missing.")
-except UnicodeDecodeError:
-    webbrowser.open('file://' + os.path.realpath(file))
-except NameError:
-    print("Error: looks like a var not defined")
+def look(file):
+    try:
+        if os.path.exists(file):
+            if os.path.isfile(file):
+                print(open(file).read())
+            else:
+                print(file + " exists, however it is not a file (looks more like a folder to me).")
+                print("Correct the path to point to an actual file.")
+        elif not os.path.exists(file):
+            print(file + " is missing.")
+    except UnicodeDecodeError:
+        webbrowser.open('file://' + os.path.realpath(file))
+    except NameError:
+        print("Error: looks like a var not defined")
+
+look(this)
